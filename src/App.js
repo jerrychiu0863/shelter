c5e4c0a5f4b65dee991b09a9bb5b6df4a288ef82
Jerry Chiu <m08632000@hotmail.com>
2021-12-08 23:26:32 +0800

    first comment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,110 @@
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [shelter, setShelter] = useState('');
+  const [kind, setKind] = useState('');
+  // const onBtnClick = () => {
+  //   setCount(count + 1);
+  // };
+  useEffect(() => {
+    // const id = 234482;
+    // fetch(
+    //   'https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&$top=10&animal_id=&animal_kind=%E8%B2%93'
+    // )
+    //   .then((res) => {
+    //     // console.log(res.json());
+    //     // console.log(res.json());
+    //     return res.json();
+    //   })
+    //   .then((data) => {
+    //     setData(data);
+    //     console.log(data);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+  }, []);
+
+  const fetchData = (shelter = '', kind = '') => {
+    fetch(
+      `https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&$top=10&animal_id=&animal_shelter_pkid=${shelter}&animal_kind=${kind}`
+    )
+      .then((res) => {
+        // console.log(res.json());
+        // console.log(res.json());
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const onShelterChange = (e) => {
+    setShelter(e.target.value);
+    fetchData(e.target.value, kind);
+  };
+
+  const onKindChange = (e) => {
+    setKind(e.target.value);
+    fetchData(shelter, e.target.value);
+  };
+  console.log(shelter);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <select onChange={onKindChange}>
+        <option value="">Type</option>
+        <option value="%E8%B2%93">Cat</option>
+      </select>
+      <select onChange={onShelterChange}>
+        <option value="">All</option>
+        <option value="48">基隆市寵物銀行</option>
+      </select>
+      <div>
+        <ul>
+          {data.map((pet) => {
+            return (
+              <li key={pet.animal_id}>
+                <p>Kind:{pet.animal_kind}</p>
+                <p>Shelter:{pet.shelter_name}</p>
+                {/* <div
+                  style={{
+                    width: '200px',
+                    height: ' 150px',
+                    overflow: 'hidden',
+                  }}
+                >
+                  <img src={pet.album_file} width="100%" height="auto" />
+                </div> */}
+                <img
+                  src={pet.album_file}
+                  width="200px"
+                  height="200px"
+                  style={{ objectFit: 'scale-down' }}
+                />
+                <div
+                  style={{
+                    backgroundImage: `url(${pet.album_file})`,
+                    width: '300px',
+                    height: '300px',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'top center',
+                  }}
+                ></div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 }
 
